b40a03f5a7c016faa39855a9f5c0a5c8d4b8059b
Mashal <zyadomar112@gmail.com>
2025-06-11 16:34:38 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar-toggle").addEventListener("click", function () {
+    document.getElementById("navbar-menu").classList.toggle("active");
+});
+window.addEventListener("load", () => {
+    document.querySelector(".hero_content").style.opacity = "1";
+    document.querySelector(".hero_image").style.opacity = "1";
+});
